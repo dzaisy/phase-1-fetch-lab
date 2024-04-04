89d7c0afa3940af7265919371d0e522de42a2f30
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-04 21:51:09 +0200

    1 passing test

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+document.addEventListener('DOMContentLoaded', fetchBooks);
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://www.anapioficeandfire.com/api/books')
+  .then(resp => {
+    if(!resp.ok) {
+      throw new Error('error with response');
+    }
+    else {
+    return resp.json();
+    }
+    
+  })
+  .then(books => {
+    renderBooks(books);
+  })
+  .catch(error => {
+    console.error('error fetching books:', error);
+  })
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+function renderBooks() {}
